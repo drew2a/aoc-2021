12b6e611b9ed5eac2e2e8c3bcd2b1bb2e7551bef
drew2a <for.drew@gmail.com>
2021-12-08 11:58:46 +0100

    Optimize day 7 part 2

--- day7_2.py
+++ day7_2.py
@@ -3,9 +3,24 @@ from pathlib import Path
 
 positions = [int(value) for value in Path(sys.argv[1]).read_text().split(',')]
 
-min_position = min(positions)
-max_position = max(positions)
+fuel_list = []
+cache = {}
 
-min_fuel = min(sum(sum(range(abs(item - position) + 1)) for item in positions) for position in
-               range(min_position, max_position + 1))
-print(f'Min fuel: {min_fuel}')
+
+def calculate_fuel_for_position(position):
+    for item in positions:
+        normal_distance = abs(item - position) + 1
+        if normal_distance in cache:
+            distance = cache[normal_distance]
+        else:
+            distance = sum(range(normal_distance))
+            cache[normal_distance] = distance
+        yield distance
+
+
+def assemble_fuel_list():
+    for position in range(min(positions), max(positions) + 1):
+        yield sum(calculate_fuel_for_position(position))
+
+
+print(f'Min fuel: {min(assemble_fuel_list())}')
