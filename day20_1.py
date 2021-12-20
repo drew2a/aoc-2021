09d998267d028f7257f12f219f47e2d0251d40ea
drew2a <for.drew@gmail.com>
2021-12-20 18:23:04 +0100

    Add day 20

---
+++ day20_1.py
@@ -0,0 +1,34 @@
+import sys
+from pathlib import Path
+
+inputs = Path(sys.argv[1]).read_text().strip()
+
+inputs = inputs.split('\n\n')
+image_enhancement_algorithm = inputs[0].replace('\n', '').strip()
+input_image = [line for line in inputs[1].split('\n') if line]
+infinite_color = '.'
+
+
+def get_pixel(x, y):
+    line = ''
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if 0 <= i < len(input_image) and 0 <= j < len(input_image):
+                line += input_image[i][j]
+            else:
+                line += infinite_color
+    pixel_index = int(line.replace('.', '0').replace('#', '1'), 2)
+    return image_enhancement_algorithm[pixel_index]
+
+
+for _ in range(2):
+    image = []
+    for i in range(-1, len(input_image) + 1):
+        line = ''
+        for j in range(-1, len(input_image) + 1):
+            line += get_pixel(i, j)
+        image.append(line)
+    input_image = image
+    infinite_color = get_pixel(-1000000, -1000000)
+
+print(f'Lit pixels count: {len([ch for ch in "".join(input_image) if ch == "#"])}')
